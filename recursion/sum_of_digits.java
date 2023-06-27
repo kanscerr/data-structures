6deb4c8f4a13a79e6c4e19fe7fd0f9b25ba61150
kanscerr <92222438+kanscerr@users.noreply.github.com>
2023-06-27 21:33:11 +0530

    sum of digits of a number

--- recursion/sum_of_digits.java
+++ recursion/sum_of_digits.java
@@ -1,5 +1,15 @@
 package recursion;
 
 public class sum_of_digits {
-
+    static int sum(int num){
+        if(num == 0){
+            return 0;
+        }
+        int sum = num%10 + sum(num/10);
+        return sum;
+    }
+    public static void main(String[] args) {
+        int num = 1234;
+        System.out.println(sum(num));
+    }
 }
