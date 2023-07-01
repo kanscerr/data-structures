93b319d75d01234bc8631472518c848fea0c51d2
kanscerr <92222438+kanscerr@users.noreply.github.com>
2023-07-01 12:23:44 +0530

    sum of natural number

---
+++ recursion/sumNaturalNumber.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class sumNaturalNumber {
+    static int sum(int n){
+       if(n==1){
+           return 1;
+       }
+       return sum(n-1)+n;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(sum(n));
+    }
+}
