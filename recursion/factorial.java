c973bad87036323c83f04cd4db435040aa75c238
kanscerr <92222438+kanscerr@users.noreply.github.com>
2023-07-01 12:24:06 +0530

    factorial of number

---
+++ recursion/factorial.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class factorial {
+    static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return fact(n-1)*n;
+    }
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}
