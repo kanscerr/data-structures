0c28026a85053363f1dd50d0391361ff776deb26
kanscerr <92222438+kanscerr@users.noreply.github.com>
2023-09-27 20:40:45 +0530

    added in-place merge sort

--- sort/merge_sort.java
+++ sort/merge_sort.java
@@ -1,6 +1,37 @@
 package sort;
 import java.util.Arrays;
 public class merge_sort{
+    //in-place merge sort
+    static void merge_inplace(int[] arr, int l, int mid, int r){
+        int i = r-mid;
+        while(l<=mid && i<=r){
+            if(arr[l] <= arr[i]){
+                l++;
+            }
+            else{
+                int temp = arr[i];
+                int idx = i;
+                while(idx != l){
+                    arr[idx] = arr[idx-1];
+                    idx--;
+                }
+                arr[l] = temp;
+                l++;
+                mid++;
+                i++;
+            }
+        }
+    }
+    static void mergesort_inplace(int[] arr, int l, int r){
+        if(l<r){
+            int mid = (l+r)/2;
+            mergesort_inplace(arr, l, mid);
+            mergesort_inplace(arr, mid+1, r);
+            merge_inplace(arr, l, mid, r);
+        }
+    }
+
+    //normal merge sort
     static void merge(int[] arr, int l, int mid, int r){
         int n1 = mid-l+1;
         int n2 = r-mid;
@@ -38,8 +69,46 @@ public class merge_sort{
         }
     }
     public static void main(String[] args){
-        int[] arr = {12, 11, 13, 5, 6, 7};
-        mergesort(arr, 0, arr.length-1);
-        System.out.println(Arrays.toString(arr));
+        int[] arr1 = {12, 11, 13, 5, 6, 7};
+        int[] arr2 = {5, 89, 21, 0, 33, 22, 87, 65};
+        mergesort_inplace(arr1, 0, arr1.length-1);
+        mergesort(arr2, 0, arr2.length-1);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static void main(String[] args){
+//        int[] arr = {12, 11, 13, 5, 6, 7};
+//        mergesort(arr, 0, arr.length-1);
+//        System.out.println(Arrays.toString(arr));
+//    }
